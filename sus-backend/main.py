cac8c2b878709866c6d1743b4ae30ebdff94aa2d
RakshamSharma <rakshamshar@gmail.com>
2026-05-20 01:16:41 +0530

    Production

--- sus-backend/main.py
+++ sus-backend/main.py
@@ -14,7 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000"
+        "http://localhost:3000", 
+        "https://suscan.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
